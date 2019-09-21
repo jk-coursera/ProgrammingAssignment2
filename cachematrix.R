528e4f44875db7578763a602e4d9e5521643109b
Jon Kivinen <54950943+jk-coursera@users.noreply.github.com>
2019-09-21 11:55:01 -0400

    Implemented makeMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Wraps matrix 'x' with caching functionality.
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # Create variable for caching result of 'solve(x)'
+    cache_solve <- NULL
+    
+    # Setter for matrix 'x'. Resets cached solution.
+    set <- function(y) {
+        x <<- y
+        cache_solve <<- NULL
+    }
+    
+    # Getter for matrix 'x'.
+    get <- function() x
+    
+    # Sets cached solution.
+    setsolution <- function(solution) cache_solve <<- solution
+    
+    # Gets cached solution.
+    getsolution <- function() cache_solve
+    
+    # Return wrapped object.
+    list(set = set, get = get,
+         setsolution = setsolution,
+         getsolution = getsolution)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the solution to matrix 'x' and caches the result.
+## Subsequent calls to 'cacheSolve' will return the cached solution.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Get the cached solution for matrix 'x'.
+    solution <- x$getsolution()
+    
+    if(!is.null(solution))
+    {
+        message("cacheSolve: Returning cached solution.")
+        return(solution)
+    }
+    
+    # Get raw matrix from 'x', compute the solution, and cache the result.
+    data <- x$get()
+    solution <- solve(data)
+    x$setsolution(solution)
+    
+    # Return the computed solution
+    solution
 }
